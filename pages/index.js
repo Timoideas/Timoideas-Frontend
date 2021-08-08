9687a5ad2950bc4b02fb2c7589c860967a92e072
Timoideas <Timoideas@hotmail.com>
2021-08-08 16:47:54 -0500

    Repository created successfully.

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import style from 'styles/pages/index.module.css';
+import Head_Main from 'heads/main.head';
+import {
+  Body,
+  Section,
+  Content,
+} from 'components/timoideas/Timoideas.components';
+import BienvenidaSockets from 'components/sockets/SocketsSaludo.component';
+
+export default function Index() {
+  return (
+    <>
+      <Head_Main />
+      <Body>
+        <Section>
+          <Content center flex={0.5}>
+            <h1 className={style.Title}>Frontend</h1>
+            <BienvenidaSockets />
+          </Content>
+        </Section>
+      </Body>
+    </>
+  );
+}
