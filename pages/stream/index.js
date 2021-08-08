0e1327c4ad86eb5d1acd51bfaf06a5e631f60268
Timoideas <Timoideas@hotmail.com>
2021-08-08 18:13:29 -0500

    Api

--- pages/stream/index.js
+++ pages/stream/index.js
@@ -4,9 +4,42 @@ import {
   Section,
   Body,
   Content,
-  Boton_1,
 } from 'components/timoideas/Timoideas.components';
+import { useEffect } from 'react';
+import { socket } from 'sockets/Index.socket';
 export default function Stream() {
+  useEffect(() => {
+    const audio = new Audio();
+    var constraints = { audio: true };
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function (mediaStream) {
+        var mediaRecorder = new MediaRecorder(mediaStream);
+        mediaRecorder.onstart = function () {
+          this.chunks = [];
+        };
+        mediaRecorder.ondataavailable = function (e) {
+          this.chunks.push(e.data);
+        };
+        mediaRecorder.onstop = function () {
+          var blob = new Blob(this.chunks, { type: 'audio/ogg; codecs=opus' });
+          socket.emit('radio', blob);
+        };
+
+        // Start recording
+        mediaRecorder.start();
+
+        setInterval(function () {
+          mediaRecorder.stop();
+          mediaRecorder.start();
+        }, 1000);
+      });
+    socket.on('voice', function (arrayBuffer) {
+      var blob = new Blob([arrayBuffer], { type: 'audio/ogg; codecs=opus' });
+      audio.src = window.URL.createObjectURL(blob);
+      audio.play();
+    });
+  }, []);
   return (
     <>
       <Head />
